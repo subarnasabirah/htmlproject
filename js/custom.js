be85d1316bf8142bd1d1575c5dba790f9a752a08
unknown <you@example.com>
2020-08-10 17:40:20 +0600

    carousel completed

--- js/custom.js
+++ js/custom.js
@@ -13,6 +13,28 @@ function myFunction(){
 
 }
 
+$('.dot-list').owlCarousel({
+    loop:true,
+    margin: 30,                              
+    nav:false,
+    autoplayHoverPause:true,
+    dots: true,
+    autoplay: 1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        700:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
 
 
 
